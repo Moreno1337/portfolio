5ad21c5ef605acd0ccd67b5c6c3f5e58ea9d7590
João Pedro Moreno <105954332+Moreno1337@users.noreply.github.com>
2022-07-19 18:48:20 -0300

    See More button addition
    See More button addition and its dynamic behaviors

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 const menuSanduiche = document.querySelector('[data-sanduiche]');
 const navegacao = document.querySelector('.cabecalho__navegacao');
 
-menuSanduiche.addEventListener('click', () => {
-    mostraMenu();
-});
+menuSanduiche.addEventListener('click', () => mostraMenu());
 
 function mostraMenu() {
     if(navegacao.classList.length == 2) {
@@ -11,4 +9,35 @@ function mostraMenu() {
     } else {
         navegacao.classList.add('cabecalho__esconde');
     }
+}
+
+const botaoVerMais = document.querySelector('.projetos__ver-mais');
+const projetos = document.querySelectorAll('[data-projeto]')
+
+botaoVerMais.addEventListener('click', () => {
+    if(botaoVerMais.textContent === 'Ver Mais') {
+        botaoVerMais.textContent = 'Ver Menos';
+    } else {
+        botaoVerMais.textContent = 'Ver Mais';
+    }
+    mostraMaisProjetos();
+})
+
+function mostraMaisProjetos() {
+    projetos.forEach( (elemento) => {
+        if(elemento.classList.length == 2) {
+            if(elemento.dataset.projeto == 'celular') {
+                elemento.classList.remove('esconde-celular');
+            } else {
+                elemento.classList.remove('esconde-tablet');
+            }
+        
+        } else {
+            if(elemento.dataset.projeto == 'celular') {
+                elemento.classList.add('esconde-celular');  
+            } else {
+                elemento.classList.add('esconde-tablet');
+            }
+        }
+    });
 }
